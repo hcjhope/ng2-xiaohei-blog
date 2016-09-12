36c56d9e3fc87ca37e8ca765cd8d63418745463f
lizhiqianduan <1024669228@qq.com>
2016-09-12 23:52:42 +0800

    add project seed

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xiaohei-blog',
+  template: `
+    <h1>{{title}}</h1>
+  `
+})
+export class AppComponent {
+  title = 'ng2 xiaohei blog , hello world !';
+}
